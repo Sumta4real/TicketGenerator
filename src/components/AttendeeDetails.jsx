583476928c657a77e6a64142b0315cf3d1e957bb
Sumayyah <sumty4deen@gmail.com>
2025-02-14 20:22:44 +0100

    updated

--- src/components/AttendeeDetails.jsx
+++ src/components/AttendeeDetails.jsx
@@ -1,121 +1,171 @@
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCloudDownloadAlt } from "@fortawesome/free-solid-svg-icons";
-import { useDropzone } from "react-dropzone";
 import { useState } from "react";
 
 export default function AttendeeDetails(props) {
-    const [avatarUrl, setAvatarUrl] = useState(null)
-    const [loading, setLoading] = useState(false)
+  const [avatarUrl, setAvatarUrl] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    handleFileUpload(file);
+  };
 
-    const { getRootProps, getInputProps } = useDropzone({
-        accept: "image/*",
-        onDrop: (acceptedFiles) => handleFileUpload(acceptedFiles[0]),
-    });
+  const handleFileUpload = (file) => {
+    setLoading(true);
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", "ImageAvatarForConference"); // replace with your preset
 
-    const handleFileUpload = (file) => {
-        setLoading(true);
-        const formData = new FormData()
-        formData.append("file", file)
-        formData.append("upload_preset", "ImageAvatarForConference"); // replace with your preset
+    fetch("https://api.cloudinary.com/v1_1/dbrseiwly/image/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setAvatarUrl(data.secure_url);
+        props.setValue("avatar", data.secure_url);
+      })
+      .catch((err) => {
+        console.error("Error uploading file:", err);
+        alert("File upload failed, please try again.");
+      })
+      .finally(() => setLoading(false));
+  };
 
-        fetch("https://api.cloudinary.com/v1_1/dbrseiwly/image/upload", {
-            method: "POST",
-            body: formData,
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                setAvatarUrl(data.secure_url)
-                props.setValue("avatar", data.secure_url)
-            })
-            .catch((err) => {
-                console.error("Error uploading file:", err)
-                alert("File upload failed, please try again.")
-            })
-            .finally(() => setLoading(false));
-    };
-
-    return ( 
-        <div className="pageBody">
-            <form onSubmit={props.handleSubmit}>
-                <div className="pageTitle">
-                    <p className="text"> Attendee Details </p>
-                    <p>Step 2/3 </p>
-                </div>
-                <div className="progressBar"></div>
-                <div className="container">
-                <div className="segment">
-                    <label htmlFor="avatar" className="smallText">Upload Profile Photo</label>
-                    <div {...getRootProps()} className="imgUploaderContainer" tabIndex='0' role="button" aria-label="Upload an avatar of choice">
-                        <input {...getInputProps()} className="hide" id="avatar" type="file" />
-                        <div className="imgUploader" style={{ padding: avatarUrl ? "0" : "24px" }}>
-                            {avatarUrl ? (
-                                <img className="imagePreview" src={avatarUrl} alt="Profile" />
-                                
-                            ) : (
-                                <>
-                                    <FontAwesomeIcon className="icon" icon={faCloudDownloadAlt} />
-                                    <p className="">Drag & drop or click to upload</p>
-                                </>
-                            )}
-                        </div>
-                    </div>
-                    {loading && <p className="smallText">Loading...</p>}
-                    {props.errors.avatar && <p className="error" role="alert">Please upload a profile picture.</p>}
-                </div>
-
-                    <div className="horizontalLine"></div>
-
-                    <div>
-                        <label htmlFor="fullName" className="smallText" tabIndex='1'  aria-label="Enter your name">Enter your name</label>
-                        <input
-                            type="text"
-                            name="fullName"
-                            id="fullName"
-                            className="segment input"
-                            {...props.register("fullName", { required: "Please fill in your name" })}
-                            aria-describedby="nameError"
-                        />
-                        {props.errors.fullName && <p className="error" role="alert">{props.errors.fullName.message}</p>}
-                    </div>
-
-                    <div>
-                        <label htmlFor="email" className="smallText" tabIndex='2'  aria-label="Enter your email">Enter your email*</label>
-                        <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            className="segment input"
-                            placeholder="✉ hello@avioflagos.io"
-                            {...props.register("email", { required: "Please fill in your email" })}
-                            aria-describedby="emailError"
-                        />
-                        {props.errors.email && <p className="error" role="alert">{props.errors.email.message}</p>}
-                    </div>
-
-                    <div>
-                        <label htmlFor="specialRequest" className="smallText" tabIndex='3'  aria-label="Enter your special request">Special Request?</label>
-                        <textarea
-                            name="specialRequest"
-                            id="specialRequest"
-                            className="segment"
-                            placeholder="Textarea"
-                            cols="3"
-                            {...props.register("specialRequest")}
-                            aria-describedby="specialRequestError"
-                        />
-                        {props.errors.specialRequest && <p className="error" role="alert">{props.errors.specialRequest.message}</p>}
-                        
-
-                    </div>
-
-                    <div className="buttons">
-                        <button className="button buttonReady" type="submit" disabled={!props.isValid || !avatarUrl} onClick={props.next}>Get My Ticket</button>
-                        <button className="button" onClick={props.back}>Back</button>
-                    </div>
-                </div>
-            </form>
+  return (
+    <div className="pageBody">
+      <form onSubmit={props.handleSubmit}>
+        <div className="pageTitle">
+          <p className="text">Attendee Details</p>
+          <p>Step 2/3</p>
         </div>
-    );
-}
+        <div className="progressBar"></div>
+        <div className="container">
+          <div className="segment">
+            <label
+              htmlFor="avatar"
+              className="smallText"
+              tabIndex="0"
+              aria-label="Upload an avatar of choice"
+            >
+              Upload Profile Photo
+            </label>
+            <div className="imgUploaderContainer" tabIndex="0" role="button">
+              <input
+                type="file"
+                id="avatar"
+                className="hide"
+                onChange={handleFileChange}
+              />
+              <div
+                className="imgUploader"
+                style={{ padding: avatarUrl ? "0" : "24px" }}
+              >
+                {avatarUrl ? (
+                  <img
+                    className="imagePreview"
+                    src={avatarUrl}
+                    alt="Profile"
+                  />
+                ) : (
+                  <>
+                    <FontAwesomeIcon
+                      className="icon"
+                      icon={faCloudDownloadAlt}
+                    />
+                    <p className="">Drag & drop or click to upload</p>
+                  </>
+                )}
+              </div>
+            </div>
+            {loading && <p className="smallText">Loading...</p>}
+            {props.errors.avatar && (
+              <p className="error" role="alert">
+                Please upload a profile picture.
+              </p>
+            )}
+          </div>
+
+          <div className="horizontalLine"></div>
+
+          <div>
+            <label
+              htmlFor="fullName"
+              className="smallText"
+              tabIndex="1"
+              aria-label="Enter your name"
+            >
+              Enter your name
+            </label>
+            <input
+              type="text"
+              name="fullName"
+              id="fullName"
+              className="segment input"
+              {...props.register("fullName", {
+                required: "Please fill in your name",
+              })}
+              aria-describedby="nameError"
+            />
+            {props.errors.fullName && (
+              <p className="error" role="alert">
+                {props.errors.fullName.message}
+              </p>
+            )}
+          </div>
+
+          <div>
+            <label
+              htmlFor="email"
+              className="smallText"
+              tabIndex="2"
+              aria-label="Enter your email"
+            >
+              Enter your email*
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="segment input"
+              placeholder="✉ hello@avioflagos.io"
+              {...props.register("email", {
+                required: "Please fill in your email",
+              })}
+              aria-describedby="emailError"
+            />
+            {props.errors.email && (
+              <p className="error" role="alert">
+                {props.errors.email.message}
+              </p>
+            )}
+          </div>
+
+          <div>
+            <label
+              htmlFor="specialRequest"
+              className="smallText"
+              tabIndex="3"
+              aria-label="Enter your special request"
+            >
+              Special Request?
+            </label>
+            <textarea
+              name="specialRequest"
+              id="specialRequest"
+              className="segment"
+              placeholder="Textarea"
+              cols="3"
+              {...props.register("specialRequest")}
+              aria-describedby="specialRequestError"
+            >
+            </textarea>
+            {props.errors.specialRequest && (
+              <p className="error" role="alert">
+                {props.errors.specialRequest.message}</p>)}
+            
+</div>
+</div>
+</form>
+</div> )}
